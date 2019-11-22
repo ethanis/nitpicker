3c4eba2bef15aae2948cb791e7250282d29e7506
Ethan Dennis <erdennis13@gmail.com>
2019-11-22 13:53:47 -0800

    Create octokit in action

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 import { getConfiguredComments } from './services';
 
 async function run() {
@@ -15,6 +16,12 @@ async function run() {
     }
 
     console.log(`There are ${comments.length} comments configured`);
+
+    const token = core.getInput('token');
+    const octokit = new github.GitHub(token);
+
+    console.log('Instantiated new octokit');
+    console.log(octokit);
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
