371a7ba702e4a4dac6599fa89ffb582ee23155f6
Ethan Dennis <erdennis13@gmail.com>
2019-11-22 15:16:32 -0800

    Return changed files in a commit

--- src/services/changes.ts
+++ src/services/changes.ts
@@ -20,28 +20,33 @@ export async function getChangedFiles(
 async function getChangedFilesFromSha(
   octokit: github.GitHub
 ): Promise<string[]> {
-  const pullRequest = github.context.payload;
-  console.log(pullRequest);
+  const beforeSha = github.context.payload.before;
+  const afterSha = github.context.payload.after;
+  const owner = github.context.payload.owner?.owner?.name;
+  const repo = github.context.payload.repository?.name;
 
-  if (!pullRequest) {
+  console.log('Before', beforeSha);
+  console.log('After', afterSha);
+
+  if (!beforeSha || !afterSha || !repo || !owner) {
     return [];
   }
 
-  return [];
-  // const listFilesResponse = await octokit.pulls.listFiles({
-  //   owner: github.context.repo.owner,
-  //   repo: github.context.repo.repo,
-  //   pull_number: pullRequest.number
-  // });
+  const listFilesResponse = await octokit.repos.compareCommits({
+    owner: owner,
+    repo: repo,
+    base: beforeSha,
+    head: afterSha
+  });
 
-  // const changedFiles = listFilesResponse.data.map(f => f.filename);
+  const changedFiles = listFilesResponse.data.files.map(f => f.filename);
 
-  // core.debug('found changed files:');
-  // for (const file of changedFiles) {
-  //   core.debug('  ' + file);
-  // }
+  core.debug('found changed files:');
+  for (const file of changedFiles) {
+    core.debug('  ' + file);
+  }
 
-  // return changedFiles;
+  return changedFiles;
 }
 
 async function getChangedFilesFromPR(
