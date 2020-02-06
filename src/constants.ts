d8f6b1bfce8ecf3c06a06a40793c7096edd844c5
Ethan Dennis <erdennis13@gmail.com>
2020-02-05 16:50:08 -0800

    Reduce length of comment service

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export module Constants {
+  export const Author: string = 'github-actions[bot]';
+  export const CannedTextSeparator: string = '\n--------------\n_Caused by:_\n';
+  export const ResolvedText: string = '\n--------------\n_Resolved_\n';
+}
