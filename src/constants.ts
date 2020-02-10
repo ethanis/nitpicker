42ad55208c698e8d7c22e593e265590c45324036
Ethan Dennis <erdennis13@gmail.com>
2020-02-10 14:57:07 -0800

    Add blocking text to blocking comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
 export module Constants {
   export const Author: string = 'github-actions[bot]';
+  export const BlockingText: string =
+    '\n__note__: this is a [blocking](https://github.com/ethanis/nitpicker#blocking-comments) comment.\n';
   export const CannedTextSeparator: string = '\n--------------\n_Caused by:_\n';
   export const ResolvedText: string = '\n--------------\n_Resolved_\n';
 }
--- src/services/configReader.ts
+++ src/services/configReader.ts
@@ -1,14 +1,18 @@
 import * as fs from 'fs';
 import { Comment } from '../models';
 import { safeLoad } from 'js-yaml';
+import { Constants } from '../constants';
 
 export function getConfiguredComments(filePath: string): Comment[] {
   const contents = fs.readFileSync(filePath, 'utf8');
   const yaml: Comment[] = safeLoad(contents);
 
-  const comments: Comment[] = yaml.filter(
-    y => y.markdown && y.pathFilter?.length > 0
-  );
+  const comments: Comment[] = yaml
+    .filter(y => y.markdown && y.pathFilter?.length > 0)
+    .map(c => ({
+      ...c,
+      markdown: `${c.markdown}${c.blocking ? Constants.BlockingText : ''}`
+    }));
 
   return comments;
 }
