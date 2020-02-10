24030b52bbe97e3f29ac60720c9228f18435e9b1
Ethan Dennis <erdennis13@gmail.com>
2020-02-10 15:03:14 -0800

    Unit tests for blocking text

--- __tests__/configReader.test.ts
+++ __tests__/configReader.test.ts
@@ -1,5 +1,6 @@
 import * as path from 'path';
 import { getConfiguredComments } from '../src/services';
+import { Constants } from '../src/constants';
 
 test('read valid config file', () => {
   const configFile = './data/valid-nitpicks.yml';
@@ -17,4 +18,18 @@ test('read invalid config file', () => {
   const comments = getConfiguredComments(inputPath);
 
   expect(comments.length).toEqual(0);
+});
+
+test('blocking comment text', () => {
+  const configFile = './data/blocking-nitpicks.yml';
+  const inputPath = path.join(__dirname, configFile);
+
+  const comments = getConfiguredComments(inputPath);
+  for (const comment of comments) {
+    if (comment.blocking) {
+      expect(comment.markdown).toContain(Constants.BlockingText);
+    } else {
+      expect(comment.markdown).not.toContain(Constants.BlockingText);
+    }
+  }
 });
